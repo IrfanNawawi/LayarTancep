9beb7c9d41a627335d574018b5bcc3bfdf1eaaef
IrfanNawawi <irfnawi@gmail.com>
2023-03-25 00:51:40 +0700

    [IMPROVE] Add Feature Single Activity Pattern

--- app/src/main/java/id/heycoding/layartancep/MainActivity.kt
+++ app/src/main/java/id/heycoding/layartancep/MainActivity.kt
@@ -1,11 +1,25 @@
 package id.heycoding.layartancep
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import id.heycoding.layartancep.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val activityMainBinding get() = _activityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding?.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.frag_container_main) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
